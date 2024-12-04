7ccd4a0e3e3839b2146833388e4d767101234738
arshad-jamil-01 <arshadjamil533@gmail.com>
2024-12-04 12:28:05 +0530

    first Commit

---
+++ src/MovieZone.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Movie from './MovieZone/Movie'
+
+const MovieZone = () => {
+  return (
+    <>
+      <Movie/>
+    </>
+  )
+}
+
+export default MovieZone
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+// import { StrictMode } from 'react'
+ import { createRoot } from 'react-dom/client'
+import './index.css'  
+
+
+
+import MovieZone from './MovieZone.jsx'
+
+createRoot(document.getElementById("root")).render(
+  <MovieZone/>
+)
+
+
+
+
+// createRoot(document.getElementById('root')).render(
+//     <React1/>
+//   // <StrictMode>
+//   //   <ReactRouter/>
+//   // </StrictMode>
+//   )
+
+
